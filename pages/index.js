4fcebc7ad53c7ac4c3fe0ef37a2f1ef0cc5acd57
Marek <marek@macbook-air-marek.home>
2023-03-31 17:15:09 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import MainView from '@/components/MainView'
 import Header from '@/components/Header'
 import MainSection from '@/components/MainSection'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] }) 
+
 
 export default function Home() {
   return (
